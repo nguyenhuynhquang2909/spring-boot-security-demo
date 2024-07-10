42fa8a89e5c8c65f77e9b5b71e8a9581232fa530
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2024-07-10 14:51:23 +0700

    Update user entities

---
+++ src/main/java/com/quang/security/user/Role.java
@@ -0,0 +1,6 @@
+package com.quang.security.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/quang/security/user/User.java
@@ -0,0 +1,81 @@
+package com.quang.security.user;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    // Implement UserDetails methods
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email; // Assuming email is used as the username
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true; // Implement your logic
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true; // Implement your logic
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true; // Implement your logic
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true; // Implement your logic
+    }
+}
